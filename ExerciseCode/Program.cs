9aa1672e2f5a0320a422374cbbf7f69abfa0fe55
bingue <glorylife17@gmail.com>
2021-11-03 06:55:20 +0800

    硬幣問題
    現有硬幣六種，分別為1元、5元、10元、20元、50元、100元，
    我們想以最少的硬幣支付 A。
    到底需要出幾枚硬幣呢?

--- ExerciseCode/Program.cs
+++ ExerciseCode/Program.cs
@@ -6,7 +6,7 @@ namespace ExerciseCode
     {
         static void Main(string[] args)
         {
-            var fun = new Course2.Function4();
+            var fun = new Course2.Function5();
             fun.test();
            
         }
