afd46f690380d58f1c8995edca7ab05f9d83cd48
bingue <glorylife17@gmail.com>
2021-11-10 21:54:48 +0800

    Fence Repair (POJ 3253)
    給予一大塊木板，要切成N小塊的子木板，每個子木板有各自的Li (i = 1 ... N)長度，
    每次切一刀成兩半時，需花費的成本是被切之前的長度。求切割最小的花費成本。
    比如範例輸入N = 3，要的子木板長度各為8、5、8，而切第一刀之前(分成13 + 8)的長度是21，
    切第二刀之前的長度是13(分成8 + 5)，所以最小的成本是21 + 13 = 34。

--- ExerciseCode/Program.cs
+++ ExerciseCode/Program.cs
@@ -6,7 +6,7 @@ namespace ExerciseCode
     {
         static void Main(string[] args)
         {
-            var fun = new Course2.Function8();
+            var fun = new Course2.Function9();
             fun.test();
            
         }
