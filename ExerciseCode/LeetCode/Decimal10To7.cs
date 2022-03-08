80ca807d80f8aeb4bc0201b6446b1a5301250384
bingue <glorylife17@gmail.com>
2022-03-08 23:06:46 +0800

    [LeetCode] Decimal 10 To 7

---
+++ ExerciseCode/LeetCode/Decimal10To7.cs
@@ -0,0 +1,52 @@
+﻿using ExerciseCode.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciseCode.LeetCode
+{
+    class Decimal10To7 : IFunction
+    {
+        public void solve1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void solve2()
+        {
+            Console.WriteLine(getDecimal7(100));
+            Console.WriteLine(getDecimal7(-7));
+        }
+
+        private string getDecimal7(int value)
+        {
+            var n = "";
+            var decimalVal = 7;
+            var decimalWord = "";
+
+            if (value < 0)
+            {
+                value = -1 * value;
+                n = "-";
+            }
+
+            while (value > 0)
+            {
+                var mod = value % decimalVal;
+                decimalWord = mod.ToString() + decimalWord;
+                value = value / decimalVal;
+            }
+           
+
+            return $"{n}{decimalWord}";
+        }
+
+        public void test()
+        {
+            Console.WriteLine("LeetCode Decimal 10 To 7:");
+            solve2();
+
+            Console.WriteLine("");
+        }
+    }
+}
