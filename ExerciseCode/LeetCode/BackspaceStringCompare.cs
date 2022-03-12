e6350bfc1665c3075bc05fecb91df827ddf610bd
bingue <glorylife17@gmail.com>
2022-03-13 07:07:59 +0800

    [LeetCode] Backspace String Compare

---
+++ ExerciseCode/LeetCode/BackspaceStringCompare.cs
@@ -0,0 +1,58 @@
+﻿using ExerciseCode.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciseCode.LeetCode
+{
+    class BackspaceStringCompare : IFunction
+    {
+        public void solve1()
+        {
+            var isSame1 = compareString("ab#c", "ad#vv##c");
+            Console.WriteLine($"LeetCode ab#c == ad#c: {isSame1}");
+
+        }
+
+        private bool compareString(string str1, string str2)
+        {
+            var word1 = breakspace(str1);
+            var word2 = breakspace(str2);
+            return word1 == word2;
+        }
+
+        private string breakspace(string str1)
+        {
+            var res = "";
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if(str1[i]!='#')
+                {
+                    res += str1[i];
+                }
+                else 
+                {
+                    if (res.Length > 0)
+                    {
+                        res = res.Remove(res.Length - 1);
+                    }
+                }
+            }
+            return res;
+        }
+
+        public void solve2()
+        {
+           
+        }
+
+        public void test()
+        {
+            Console.WriteLine("LeetCode Backspace String Compare:");
+            solve1();
+
+            Console.WriteLine("");
+        }
+    }
+}
--- ExerciseCode/Program.cs
+++ ExerciseCode/Program.cs
@@ -9,15 +9,18 @@ namespace ExerciseCode
         {
             //new LeetCode.SingleNumber().test();
             //new LeetCode.HappyNumber().test();
-            new LeetCode.MaximunSubarray().test();
+            //new LeetCode.MaximunSubarray().test();
             //new LeetCode.MoveZeroes().test();
             //new LeetCode.BestTimeToBuyAndSellStock_II().test();
             //new LeetCode.Decimal10To7().test();
             //new LeetCode.GroupAnagrams().test();
-
             //new LeetCode.CountingElements().test();
             //new LeetCode.MergeTwoSortedLists().test();
             //new LeetCode.MiddleOfTheLinkedList().test();
+            new LeetCode.BackspaceStringCompare().test();
+
+
+            //new Knowledge.DemailCalculate();
 
         }
 
